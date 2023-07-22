0854014b84a02d5e65bdc75f49c3dbd95067a472
clockworkRocker <primus2011@gmail.com>
2023-07-22 14:17:22 +0300

    Saved changes

--- headers/src/operations/nodesetop.h
+++ headers/src/operations/nodesetop.h
@@ -1,4 +1,26 @@
 #ifndef MDG_NODESETOP_H
 #define MDG_NODESETOP_H
+namespace mdg {
+template <typename GraphType>
+class NodeSetIterator : public internal::traits<GraphType>::nodeop_t {
+ public:
+};
 
+template <typename GraphType>
+class NodeSetOp {
+ public:  // * Types
+  typedef typename NodeSetIterator<GraphType> iterator;
+  typedef internal::traits<GraphType>::nodeop_t nodeop;
+
+ public:  // * Iterators because those are sexy!
+  iterator begin();
+  iterator end();
+
+ public:  // * Access methods
+  nodeop operator[](int index);
+
+ private:
+  std::vector<unsigned> indices;
+};
+}  // namespace mdg
 #endif  // MDG_NODESETOP_H
